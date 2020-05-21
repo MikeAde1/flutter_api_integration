d284532eb965ce4e8e7ab4e292a6dc792fcb32c6
michael <michael@initsng.com>
2020-05-21 16:38:01 +0100

    Set up home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapiintegration/MyHomePageState.dart';
 
-import 'api/services.dart';
-import 'data/PostCover.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,83 +28,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Api demo'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  callAPI(){
-    Post post = Post(
-        title: 'This is a test',
-        userId: 1,
-        id: 101,
-        completed: false
-    );
-    createPost(post).then((response){
-      if(response.statusCode > 200)
-        print(response.body);
-      else
-        print(response.statusCode);
-    }).catchError((error){
-      print('error : $error');
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: FutureBuilder<List<Post>>(
-        future: getAllPosts(),
-        builder: (context, snapshot){
-         //callAPI();
-         if(snapshot.connectionState == ConnectionState.done) {
-           if(snapshot.hasError){
-             return Text("Error");
-           }
-
-           return Padding(
-             padding: EdgeInsets.all(10),
-             child: Text('Title from Post JSON : ${snapshot.data.getRange(0, 10).toList()[0].title}'),
-           );
-         } else {
-           return Center(child: CircularProgressIndicator());
-         }
-        }
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: callAPI(),
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: HomePage(),
     );
   }
 }
